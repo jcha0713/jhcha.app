d076c090697a01c8619c6b19cfcaffe6db20bcf6
jcha0713 <joocha0713@gmail.com>
2021-10-22 21:03:30 -0700

    Basic tailwind css config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
-};
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['PhantomSans', 'sans-serif'],
+      },
+    },
+  },
+}
