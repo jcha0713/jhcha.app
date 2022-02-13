57832cbb3257cffc35e56ed12f1b3be9b67687d0
jcha0713 <joocha0713@gmail.com>
2022-02-13 14:00:24 -0800

    change colorscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,28 @@ module.exports = {
       },
       colors: {
         // Theme colors
-        primary: 'var(--primary-color)',
+        'primary-100': '#eff2f6',
+        'primary-200': '#dee5ed',
+        'primary-300': '#ced8e4',
+        'primary-400': '#bdcbdb',
+        'primary-500': '#adbed2',
+        'primary-600': '#9cb1c9',
+        'primary-700': '#8ea3be',
+        'primary-800': '#7b97b7',
+        'primary-900': '#6b8aae',
+
+        'secondary-100': '#cbe1d7',
+        'secondary-200': '#deede6',
+        'secondary-300': '#deede6',
+        'secondary-400': '#bddbcc',
+        'secondary-500': '#add2bf',
+        'secondary-600': '#9cc9b2',
+        'secondary-700': '#8cc0a5',
+        'secondary-800': '#7bb798',
+        'secondary-900': '#6bae8b',
+
+        hl: '#92d850',
+
         'light-bg': 'var(--light-bg)',
         'dark-bg': 'var(--dark-bg)',
 
