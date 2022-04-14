da6f32de5750bdc474f46e624185672548028e77
jcha0713 <joocha0713@gmail.com>
2022-04-13 17:06:56 -0700

    minimal dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   mode: 'jit',
-  darkMode: 'class',
+  // darkMode: 'class',
   content: [
     './public/**/*.html',
     './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
