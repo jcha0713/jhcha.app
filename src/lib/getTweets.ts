24fd76795c8a3050af6e83eb6b9de2ad9f55c64e
joohoon <joocha0713@gmail.com>
2023-06-27 21:30:50 +0900

    crawl more infos from twitter

--- src/lib/getTweets.ts
+++ src/lib/getTweets.ts
@@ -3,29 +3,6 @@ import { PlaywrightCrawler, Dataset } from 'crawlee'
 export const getTweet = async (id: string) => {
   const crawler = new PlaywrightCrawler({
     async requestHandler({ page }) {
-      //   await page.waitForSelector('div[data-testid="tweetText"]')
-      //
-      //   const text = await page.$eval(
-      //     'div[data-testid="tweetText"]',
-      //     (el) => el.textContent
-      //   )
-      //
-      //   await page.waitForSelector('div[data-testid="Tweet-User-Avatar"]')
-      //
-      //   const profile_image_url = await page.$eval(
-      //     'div[data-testid="Tweet-User-Avatar"] .css-9pa8cd',
-      //     (el) => el.getAttribute('src')
-      //   )
-      //
-      //   await Dataset.pushData({
-      //     text: text,
-      //     author: {
-      //       profile_image_url: profile_image_url,
-      //     },
-      //   })
-      // },
-
-      // await page.waitForSelector('div[data-testid="tweetText"]')
       const text = await page
         .locator('div[data-testid="tweetText"]')
         .first()
@@ -36,10 +13,29 @@ export const getTweet = async (id: string) => {
         .first()
         .getAttribute('src')
 
+      const username = await page
+        .locator('div[data-testid="User-Name"]')
+        .getByRole('link')
+        .allInnerTexts()
+
+      const created_at = await page.getByRole('time').first().textContent()
+
+      const public_metrics = await page
+        .getByTestId('app-text-transition-container')
+        .allInnerTexts()
+
       await Dataset.pushData({
         text,
+        created_at,
         author: {
+          name: username[0],
+          username: username[1],
           profile_image_url,
+        },
+        public_metrics: {
+          retweet_count: public_metrics[0] || 0,
+          reply_count: public_metrics[1] || 0,
+          like_count: public_metrics[2] || 0,
         },
       })
     },
